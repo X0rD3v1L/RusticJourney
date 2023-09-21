00285bae46f0c4578534ade85670e8c884870c27
X0rD3v1L <sbenarjee7@gmail.com>
2023-09-22 02:21:11 +0530

    RustBook Learnings

---
+++ RustBook/2_guessing_game/guessing_game/target/debug/deps/ppv_lite86-e388e0766154c6b8.d
@@ -0,0 +1,9 @@
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustBook\2_guessing_game\guessing_game\target\debug\deps\ppv_lite86-e388e0766154c6b8.rmeta: C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustBook\2_guessing_game\guessing_game\target\debug\deps\ppv_lite86-e388e0766154c6b8.d: C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs:
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs:
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs:
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs:
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs:
