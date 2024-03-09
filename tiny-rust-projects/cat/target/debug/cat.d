76c49a5874d4afb015528db224c840712695165d
X0rD3v1L <sbenarjee7@gmail.com>
2024-03-10 01:49:34 +0530

    cat command

---
+++ tiny-rust-projects/cat/target/debug/cat.d
@@ -0,0 +1,1 @@
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\tiny-rust-projects\cat\target\debug\cat.exe: C:\Users\X0r_D3v1L\Desktop\RusticJourney\tiny-rust-projects\cat\src\main.rs
---
+++ tiny-rust-projects/cat/target/debug/deps/windows_x86_64_msvc-cb8c1221b942b236.d
@@ -0,0 +1,7 @@
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\tiny-rust-projects\cat\target\debug\deps\libwindows_x86_64_msvc-cb8c1221b942b236.rmeta: C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.4\src\lib.rs
+
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\tiny-rust-projects\cat\target\debug\deps\libwindows_x86_64_msvc-cb8c1221b942b236.rlib: C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.4\src\lib.rs
+
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\tiny-rust-projects\cat\target\debug\deps\windows_x86_64_msvc-cb8c1221b942b236.d: C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.4\src\lib.rs
+
+C:\Users\X0r_D3v1L\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.4\src\lib.rs:
