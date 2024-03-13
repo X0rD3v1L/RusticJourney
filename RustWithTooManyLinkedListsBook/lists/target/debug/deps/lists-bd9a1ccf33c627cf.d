5cd1bf0ce7d5161aa1ec01baf5677ca34900b38c
X0rD3v1L <sbenarjee7@gmail.com>
2024-03-13 18:05:46 +0530

    an ok stack

--- RustWithTooManyLinkedListsBook/lists/target/debug/deps/lists-bd9a1ccf33c627cf.d
+++ RustWithTooManyLinkedListsBook/lists/target/debug/deps/lists-bd9a1ccf33c627cf.d
@@ -1,8 +1,9 @@
-C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\liblists-bd9a1ccf33c627cf.rmeta: src\lib.rs src\first.rs
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\liblists-bd9a1ccf33c627cf.rmeta: src\lib.rs src\first.rs src\second.rs
 
-C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\liblists-bd9a1ccf33c627cf.rlib: src\lib.rs src\first.rs
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\liblists-bd9a1ccf33c627cf.rlib: src\lib.rs src\first.rs src\second.rs
 
-C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-bd9a1ccf33c627cf.d: src\lib.rs src\first.rs
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-bd9a1ccf33c627cf.d: src\lib.rs src\first.rs src\second.rs
 
 src\lib.rs:
 src\first.rs:
+src\second.rs:
--- RustWithTooManyLinkedListsBook/lists/target/debug/deps/lists-eb69be4906d7dbd0.d
+++ RustWithTooManyLinkedListsBook/lists/target/debug/deps/lists-eb69be4906d7dbd0.d
@@ -1,6 +1,7 @@
-C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-eb69be4906d7dbd0.exe: src\lib.rs src\first.rs
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-eb69be4906d7dbd0.exe: src\lib.rs src\first.rs src\second.rs
 
-C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-eb69be4906d7dbd0.d: src\lib.rs src\first.rs
+C:\Users\X0r_D3v1L\Desktop\RusticJourney\RustWithTooManyLinkedListsBook\lists\target\debug\deps\lists-eb69be4906d7dbd0.d: src\lib.rs src\first.rs src\second.rs
 
 src\lib.rs:
 src\first.rs:
+src\second.rs:
